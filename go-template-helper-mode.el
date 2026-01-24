e13b09d9176dd88e65c4bea5eb5c878242ea7e13
rch <rch-public@posteo.net>
2026-01-24 23:29:31 +0100

    docs: document MELPA install and use-package

--- go-template-helper-mode.el
+++ go-template-helper-mode.el
@@ -5,7 +5,7 @@
 ;; Author: Robert Charusta <rch-public@posteo.net>
 ;; Maintainer: Robert Charusta <rch-public@posteo.net>
 ;; URL: https://codeberg.org/rch/go-template-helper-mode
-;; Version: 1.0.2
+;; Version: 1.0.3
 ;; Keywords: tools, faces
 ;; Package-Requires: ((emacs "28.1"))
 
